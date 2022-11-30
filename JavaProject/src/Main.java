4b786e5876b8b143a984fb757364f260d30e5acb
Abisola31 <aiyeneroblessing@gmail.com>
2022-11-29 18:45:17 -0800

    Modified Java Project T14

---
+++ JavaProject/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("This is my first java code");
+        System.out.println("This definitely looks interesting.");
+        System.out.println("With time, I hope I become an expert in it");
+        System.out.println("Ok,Bye!");
+
+
+    }
+}
